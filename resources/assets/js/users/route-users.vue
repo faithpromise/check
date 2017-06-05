bd75331a4fe4e07a697a391df1f2df093b06eb90
Brad Roberts <brad@bradroberts.net>
2017-06-05 12:31:26 -0400

    Moved button location

--- resources/assets/js/users/route-users.vue
+++ resources/assets/js/users/route-users.vue
@@ -2,30 +2,15 @@
 
   <div class="Content Content--flush">
 
-    <page-header v-bind:margin="false">
+    <page-header v-bind:divider="true">
       <template slot="title">People</template>
       <template slot="actions">
+        <button class="Button" type="button" v-on:click="active_only = !active_only">{{ active_only ? 'Show Everyone' : 'Show Only Actives' }}</button>
         <router-link class="Button Button--primary" v-bind:to="{ name: 'user_new' }">Add Person</router-link>
       </template>
     </page-header>
 
-    <div class="Tabs-list">
-      <router-link class="Tabs-item" v-bind:to="{ name: 'users' }">People</router-link>
-      <a class="Tabs-item">Departments</a>
-    </div>
-
     <div class="Content-container">
-
-      <!--<ul>-->
-      <!--<li v-for="user in filtered_users">-->
-      <!--<router-link v-bind:to="{ name: 'user', params: { id: user.id } }">{{ user.name }}</router-link>-->
-      <!--</li>-->
-      <!--</ul>-->
-
-      <p>
-        <button class="Button" type="button" v-on:click="active_only = !active_only">{{ active_only ? 'Show Everyone' : 'Show Only Actives' }}</button>
-        <!--<label><input type="checkbox" v-model="active_only"> Limit to active projects</label>-->
-      </p>
 
       <div class="Projects-list">
 
