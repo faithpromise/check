8b7cbccbf4d928b076b7a5eb4a4b6861043a1957
Brad Roberts <brad@bradroberts.net>
2017-06-09 14:21:54 -0400

    Allow comment body to be null

--- database/migrations/2016_05_06_191936_create_comments_table.php
+++ database/migrations/2016_05_06_191936_create_comments_table.php
@@ -17,7 +17,7 @@ class CreateCommentsTable extends Migration {
             $table->integer('project_id')->unsigned()->nullable();
             $table->integer('user_id')->unsigned();
             $table->string('type', 100)->nullable(); // estimate, mockup, proof
-            $table->text('body');
+            $table->text('body')->nullable();
             $table->dateTime('sent_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
